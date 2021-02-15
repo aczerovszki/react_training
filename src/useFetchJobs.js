44ca56b08f55390cfa1148d32dfe89c5dd8ff28f
Donifiu <52827294+Donifiu@users.noreply.github.com>
2021-02-15 17:18:39 +0100

    Updating Cors link

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -9,7 +9,7 @@ const ACTIONS = {
 };
 
 const BASE_URL =
-  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
+  "https://cors.bridged.cc/https://jobs.github.com/positions.json";
 
 function reducer(state, action) {
   switch (action.type) {
